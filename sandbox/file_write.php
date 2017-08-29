16f9ae6cfff359f5e410b1f0be33bc6c0cc4585a
Mark Bellingham <mark.bellingham@protonmail.ch>
2017-08-29 23:29:28 +0100

    Writing to files

---
+++ sandbox/file_write.php
@@ -0,0 +1,21 @@
+<?php
+$file = 'filetest.txt';
+if($handle = fopen($file, 'w')) { // ovewrite
+
+  fwrite($handle, 'abc'); // returns number of bytes or false
+  $content = "123\n456";  // double quotess matter (with \n)
+  fwrite($handle, $content);
+
+  fclose($handle);
+} else {
+  echo "Could not open file for writing";
+}
+
+// file_put_contents: shortcut for fopen/fwrite/fclose
+// overwrites existing file by default (so be CAREFUL)
+$file = 'filetest.txt';
+$content = "111\n222\n333\n";
+if($size = file_put_contents($file, $content)) {
+  echo "A file of {$size} bytes was created.";
+}
+?>
