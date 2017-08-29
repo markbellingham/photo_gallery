5fb8b2d1fb9bc85aed47b6c760159ddc890c8ad0
Mark Bellingham <mark.bellingham@protonmail.ch>
2017-08-29 16:56:33 +0100

    Late Static Bindings

---
+++ photo_gallery/includes/database_object.php
@@ -0,0 +1,59 @@
+<?php
+// If it's going to need the database, then it's
+// probably smart to require it before we start
+require_once(LIB_PATH.DS.'database.php');
+
+class DatabaseObject {
+
+  // Common Database Methods
+  // http://www.php.net/lsb   (Late Static Bindings)
+  public static function find_all() {
+    return self::find_by_sql("SELECT * FROM " . static::$table_name);
+  }
+
+  public static function find_by_id($id = 0) {
+    global $database;
+    $result_array = self::find_by_sql("SELECT * FROM " . static::$table_name . " WHERE id = {$id} LIMIT 1");
+    return !empty($result_array) ? array_shift($result_array) : false;
+  }
+
+  public static function find_by_sql($sql = "") {
+    global $database;
+    $result_set = $database->query($sql);
+    $object_array = array();
+    while ($row = $database->fetch_array($result_set)) {
+      $object_array[] = self::instantiate($row);
+    }
+    return $object_array;
+  }
+
+  private static function instantiate($record) {
+    // Could check that the record exists and is an array
+    // Simple, long-form approach:
+    $class_name = get_called_class();
+    $object = new $class_name;
+    // $user->id         = $record['id'];
+    // $user->username   = $record['username'];
+    // $user->password   = $record['password'];
+    // $user->first_name = $record['first_name'];
+    // $user->last_name  = $record['last_name'];
+
+    // More dynamic, short-form approach
+    foreach($record as $attribute=>$value) {
+      if($object->has_attribute($attribute)) {
+        $object->$attribute = $value;
+      }
+    }
+    return $object;
+  }
+
+  private function has_attribute($attribute) {
+    // get_object_vars returns as associative array with all attributes
+    // (incl. private ones!) as the keys and their current values as the value
+    $object_vars = get_object_vars($this);
+    // We don't care about the value, we just want to know if the key exists
+    // Will return true or false
+    return array_key_exists($attribute, $object_vars);
+  }
+}
+?>
--- photo_gallery/includes/user.php
+++ photo_gallery/includes/user.php
@@ -3,32 +3,21 @@
 // probably smart to require it before we start
 require_once(LIB_PATH.DS.'database.php');
 
-class User {
+class User extends DatabaseObject {
 
+  protected static $table_name = "users";
   public $id;
   public $username;
   public $password;
   public $first_name;
   public $last_name;
 
-  public static function find_all() {
-    return self::find_by_sql("SELECT * FROM users");
-  }
-
-  public static function find_by_id($id = 0) {
-    global $database;
-    $result_array = self::find_by_sql("SELECT * FROM users WHERE id = {$id} LIMIT 1");
-    return !empty($result_array) ? array_shift($result_array) : false;
-  }
-
-  public static function find_by_sql($sql = "") {
-    global $database;
-    $result_set = $database->query($sql);
-    $object_array = array();
-    while ($row = $database->fetch_array($result_set)) {
-      $object_array[] = self::instantiate($row);
+  public function full_name() {
+    if(isset($this->first_name) && isset($this->last_name)) {
+      return $this->first_name . " " . $this->last_name;
+    } else {
+      return "";
     }
-    return $object_array;
   }
 
   public static function authenticate($username="", $password="") {
@@ -44,42 +33,5 @@ class User {
     return !empty($result_array) ? array_shift($result_array) : false;
   }
 
-  public function full_name() {
-    if(isset($this->first_name) && isset($this->last_name)) {
-      return $this->first_name . " " . $this->last_name;
-    } else {
-      return "";
-
-
-    }
-  }
-
-  private static function instantiate($record) {
-    // Could check that the record exists and is an array
-    // Simple, long-form approach:
-    $object = new self;
-    // $user->id         = $record['id'];
-    // $user->username   = $record['username'];
-    // $user->password   = $record['password'];
-    // $user->first_name = $record['first_name'];
-    // $user->last_name  = $record['last_name'];
-
-    // More dynamic, short-form approach
-    foreach($record as $attribute=>$value) {
-      if($object->has_attribute($attribute)) {
-        $object->$attribute = $value;
-      }
-    }
-    return $object;
-  }
-
-  private function has_attribute($attribute) {
-    // get_object_vars returns as associative array with all attributes
-    // (incl. private ones!) as the keys and their current values as the value
-    $object_vars = get_object_vars($this);
-    // We don't care about the value, we just want to know if the key exists
-    // Will return true or false
-    return array_key_exists($attribute, $object_vars);
-  }
 }
 ?>
