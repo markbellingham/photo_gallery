16cd75cf4ac7aaa3398c0a1f13d0f84a51dadd20
Mark Bellingham <mark.bellingham@protonmail.ch>
2017-08-30 19:24:32 +0100

    Inspecting uploaded files

--- sandbox/upload.php
+++ sandbox/upload.php
@@ -1,5 +1,8 @@
 <?php
-
+echo "<pre>";
+print_r($_FILES['file_upload']);
+echo "</pre>";
+echo "<hr />";
 ?>
 
 <html>
@@ -18,9 +21,9 @@
     You can round it unless the precision matters.
   -->
   <?php if(!empty($message)) { echo "<p>{$message}</p>"; } ?>
-  <form action="upload.php" enctype="multitype/form-data" method="POST">
+  <form action="upload.php" enctype="multipart/form-data" method="POST">
     <input type="hidden" name="MAX_FILE_SIZE" value="1000000" />
-    <input type="file" name="file-upload" />
+    <input type="file" name="file_upload" />
     <input type="submit" name="submit" value="Upload" />
   </form>
 
