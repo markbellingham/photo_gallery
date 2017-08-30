e3388b021da2b06abe8701f732000de2a677af76
Mark Bellingham <mark.bellingham@protonmail.ch>
2017-08-30 19:42:10 +0100

    Uploading errors

--- sandbox/upload.php
+++ sandbox/upload.php
@@ -1,4 +1,19 @@
 <?php
+// In an application this could be moved to a config file
+$upload_errors = array(
+  // http://www.php.net/manual/en/features.file-upload.errors.php
+  UPLOAD_ERR_OK         =>  "No errors.",
+  UPLOAD_ERR_INI_SIZE   =>  "Larger than upload_max_filesize.",
+  UPLOAD_ERR_FORM_SIZE  =>  "Larger than form MAX_FILE_SIZE.",
+  UPLOAD_ERR_PARTIAL    =>  "partial upload.",
+  UPLOAD_ERR_NO_FILE    =>  "No file.",
+  UPLOAD_ERR_NO_TMP_DIR =>  "No temporary directory.",
+  UPLOAD_ERR_CANT_WRITE =>  "Can't write to disk.",
+  UPLOAD_ERR_EXTENSION  =>  "File upload stopped by extension."
+);
+$error = $_FILES['file_upload']['error'];
+$message = $upload_errors[$error];
+
 echo "<pre>";
 print_r($_FILES['file_upload']);
 echo "</pre>";
